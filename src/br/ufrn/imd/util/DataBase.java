4e311b7dfc72403a43e94161c5499cb297561adf
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-10-12 21:53:59 -0300

    Connection data updated

--- src/br/ufrn/imd/util/DataBase.java
+++ src/br/ufrn/imd/util/DataBase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DataBase {
 	private String driver = "org.postgresql.Driver";
-	private String url = "jdbc:postgresql://localhost/postgres";
+	private String url = "jdbc:postgresql://localhost/netfrix";
 	private String user;
 	private String password;
 	private Connection connection;
@@ -16,7 +16,7 @@ public class DataBase {
 			Class.forName(this.driver);
 			this.connection = DriverManager.getConnection(this.url, user, password);
 		} catch (ClassNotFoundException e) {
-			System.out.println("Não foi possível encontrar o driver do banco: " + e.getMessage());
+			System.out.println("NÃ£o foi possÃ­vel encontrar o driver do banco: " + e.getMessage());
 		} catch (SQLException e) {
 			System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
 		}
