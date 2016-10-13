74a25d8f35ad2f31b75dc9bdd5a0670d5b5df82b
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-10-13 00:12:20 -0300

    Connection refactored

--- src/br/ufrn/imd/netfrix/util/DataBase.java
+++ src/br/ufrn/imd/netfrix/util/DataBase.java
@@ -2,23 +2,120 @@ package br.ufrn.imd.netfrix.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.sun.rowset.CachedRowSetImpl;
+
+import br.ufrn.imd.netfrix.model.User;
 
 public class DataBase {
-	private String driver = "org.postgresql.Driver";
-	private String url = "jdbc:postgresql://localhost/netfrix";
-	private String user;
-	private String password;
-	private Connection connection;
-	
-	public DataBase(String user, String password) {
-		try {
-			Class.forName(this.driver);
-			this.connection = DriverManager.getConnection(this.url, user, password);
-		} catch (ClassNotFoundException e) {
-			System.out.println("Não foi possível encontrar o driver do banco: " + e.getMessage());
-		} catch (SQLException e) {
-			System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
-		}
-	}
+	//Declare JDBC Driver
+    private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static String url = "jdbc:postgresql://localhost/netfrix";
+	private static String user	= "postgres";
+	private static String password = "postgres";
+ 
+    //Connection
+    private static Connection conn = null;
+ 
+    //Connect to DB
+    public static void dbConnect() throws SQLException, ClassNotFoundException {
+        //Setting Oracle JDBC Driver
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            throw e;
+        }
+ 
+        System.out.println("Oracle JDBC Driver Registered!");
+ 
+        //Establish the Oracle Connection using Connection String
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console" + e);
+            e.printStackTrace();
+            throw e;
+        }
+    }
+ 
+    //Close Connection
+    public static void dbDisconnect() throws SQLException {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (Exception e){
+           throw e;
+        }
+    }
+ 
+    //DB Execute Query Operation
+    public static ResultSet dbExecuteQuery(String queryStmt) throws SQLException, ClassNotFoundException {
+        //Declare statement, resultSet and CachedResultSet as null
+        Statement stmt = null;
+        ResultSet resultSet = null;
+        CachedRowSetImpl crs = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            System.out.println("Select statement: " + queryStmt + "\n");
+ 
+            //Create statement
+            stmt = conn.createStatement();
+ 
+            //Execute select (query) operation
+            resultSet = stmt.executeQuery(queryStmt);
+ 
+            //CachedRowSet Implementation
+            //In order to prevent "java.sql.SQLRecoverableException: Closed Connection: next" error
+            //We are using CachedRowSet
+            crs = new CachedRowSetImpl();
+            crs.populate(resultSet);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                //Close resultSet
+                resultSet.close();
+            }
+            if (stmt != null) {
+                //Close Statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+        //Return CachedRowSet
+        return crs;
+    }
+ 
+    //DB Execute Update (For Update/Insert/Delete) Operation
+    public static void dbExecuteUpdate(String sqlStmt) throws SQLException, ClassNotFoundException {
+        //Declare statement as null
+        Statement stmt = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            //Create Statement
+            stmt = conn.createStatement();
+            //Run executeUpdate operation with given sql statement
+            stmt.executeUpdate(sqlStmt);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeUpdate operation : " + e);
+            throw e;
+        } finally {
+            if (stmt != null) {
+                //Close statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+    }
 }
