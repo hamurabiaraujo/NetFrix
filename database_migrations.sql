ba442bdb71a38f2bf5354821f89f4304aa793554
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-10-17 17:59:56 -0300

    Migration script with create table video statement adds

--- database_migrations.sql
+++ database_migrations.sql
@@ -9,4 +9,20 @@ CREATE TABLE users (
   date_of_birth date
 );
 
+CREATE TABLE public.video
+(
+  id serial PRIMARY KEY,
+  name character varying(250),
+  description character varying(2000),
+  year integer,
+  duration integer,
+  director character varying(150),
+  main_actor character varying(150),
+  main_age integer,
+  registration_date date,
+  image character varying(150),
+  id_season integer,
+  episode integer
+);
+
 INSERT INTO users ( name, email, password, is_admin ) VALUES ( 'Netfrix Admin', 'admin@netfrix.com.br', '123123123', true );
