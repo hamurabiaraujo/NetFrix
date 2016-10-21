f243a910ae59edabfa766beda41874dfcd65d106
Luan Santos <luankjs@gmail.com>
2016-10-20 22:45:49 -0300

    Adds Users management to Dashboard

--- src/br/ufrn/imd/netfrix/view/DashboardViewController.java
+++ src/br/ufrn/imd/netfrix/view/DashboardViewController.java
@@ -1,12 +1,19 @@
 package br.ufrn.imd.netfrix.view;
 
+import java.sql.SQLException;
+
 import br.ufrn.imd.netfrix.MainApp;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class DashboardViewController {
+	@FXML
+	private Button btnAddUser;
+
 	public DashboardViewController() {
 	}
 
@@ -24,5 +31,24 @@ public class DashboardViewController {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void showUserView(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/UserView.fxml"));
+            AnchorPane userView = (AnchorPane) loader.load();
+
+            Scene scene = new Scene(userView);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+
+	@FXML
+	private void handleAddUserClick() throws ClassNotFoundException, SQLException {
+	    showUserView(new Stage());
 	}
 }
